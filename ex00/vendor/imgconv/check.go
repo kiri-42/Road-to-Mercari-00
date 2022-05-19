d276f08a77616d3e5e56f480931575919db34c58
kiri0314 <t.kirihara0314@gmail.com>
2022-05-19 18:45:31 +0900

    fix: 不要な関数をコメントアウト

--- ex00/vendor/imgconv/check.go
+++ ex00/vendor/imgconv/check.go
@@ -7,30 +7,30 @@ import (
 	"strings"
 )
 
-func checkFileContentType(path string) (bool, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return false, err
-	}
-	defer f.Close()
+// func checkFileContentType(path string) (bool, error) {
+// 	f, err := os.Open(path)
+// 	if err != nil {
+// 		return false, err
+// 	}
+// 	defer f.Close()
 
-	// 最初の512bytesを読む
-	buffer := make([]byte, 512)
-	size, err := f.Read(buffer)
-	if size == 0 {
-		return false, nil
-	}
-	if err != nil {
-		return false, err
-	}
+// 	// 最初の512bytesを読む
+// 	buffer := make([]byte, 512)
+// 	size, err := f.Read(buffer)
+// 	if size == 0 {
+// 		return false, nil
+// 	}
+// 	if err != nil {
+// 		return false, err
+// 	}
 
-	// 有効なMIMEタイプでない場合は「application/octet-stream」が返る
-	if http.DetectContentType(buffer) == "application/octet-stream" {
-		return false, nil
-	}
+// 	// 有効なMIMEタイプでない場合は「application/octet-stream」が返る
+// 	if http.DetectContentType(buffer) == "application/octet-stream" {
+// 		return false, nil
+// 	}
 
-	return true, nil
-}
+// 	return true, nil
+// }
 
 // 引数の拡張子がjpgか確認する関数
 func CheckJpg(path string) error {
@@ -38,13 +38,13 @@ func CheckJpg(path string) error {
 		return errors.New("error: " + path + " is not a valid file")
 	}
 
-	ok, err := checkFileContentType(path)
-	if err != nil {
-		return err
-	}
-	if !ok {
-		return errors.New("error: " + path + " is not a valid file")
-	}
+	// ok, err := checkFileContentType(path)
+	// if err != nil {
+	// 	return err
+	// }
+	// if !ok {
+	// 	return errors.New("error: " + path + " is not a valid file")
+	// }
 
 	return nil
 }
