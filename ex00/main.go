cbecad1fad142b7b743af627933f6bc65149e734
kiri0314 <t.kirihara0314@gmail.com>
2022-05-19 22:56:34 +0900

    ref: ex01を削除

--- ex00/main.go
+++ ex00/main.go
@@ -6,19 +6,25 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"flag"
 
 	"imgconv"
 )
 
 func main() {
-	args := os.Args
+	var (
+		ext = flag.String("e", ".png", "Specify the extension")
+	)
+	flag.Parse()
+
+	args := flag.Args()
 	// 引数チェック
 	if err := checkArg(args); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
 	}
 
-	dir := args[1]
+	dir := args[0]
 	// 画像ファイルのpathを取得
 	paths, err := getPaths(dir)
 	if err != nil {
@@ -28,18 +34,18 @@ func main() {
 
 	// 画像変換処理
 	for _, path := range paths {
-		if err := imgconv.Convert(path, ".jpg", ".png"); err != nil {
+		if err := imgconv.Convert(path, filepath.Ext(path), *ext); err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 		}
 	}
 }
 
 func checkArg(args []string) error {
-	if len(args) != 2 {
+	if len(args) != 1 {
 		return errors.New("error: invalid argument")
 	}
 
-	dir := args[1]
+	dir := args[0]
 	if f, err := os.Stat(dir); os.IsNotExist(err) || !f.IsDir() {
 		return errors.New("error: " + dir + ": no such dir")
 	}
