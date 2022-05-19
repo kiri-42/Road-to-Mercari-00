d4910135f7bf192353c065daae9489d266fd53ca
kiri0314 <t.kirihara0314@gmail.com>
2022-05-19 20:01:50 +0900

    ref: 余計なコードを削除

--- ex00/vendor/imgconv/check.go
+++ ex00/vendor/imgconv/check.go
@@ -2,49 +2,14 @@ package imgconv
 
 import (
 	"errors"
-	// "net/http"
-	// "os"
 	"strings"
 )
-
-// func checkFileContentType(path string) (bool, error) {
-// 	f, err := os.Open(path)
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	defer f.Close()
-
-// 	// 最初の512bytesを読む
-// 	buffer := make([]byte, 512)
-// 	size, err := f.Read(buffer)
-// 	if size == 0 {
-// 		return false, nil
-// 	}
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	// 有効なMIMEタイプでない場合は「application/octet-stream」が返る
-// 	if http.DetectContentType(buffer) == "application/octet-stream" {
-// 		return false, nil
-// 	}
-
-// 	return true, nil
-// }
 
 // 引数の拡張子がjpgか確認する関数
 func CheckExt(path, ext string) error {
 	if !strings.HasSuffix(path, ext) {
 		return errors.New("error: " + path + " is not a valid file")
 	}
-
-	// ok, err := checkFileContentType(path)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !ok {
-	// 	return errors.New("error: " + path + " is not a valid file")
-	// }
 
 	return nil
 }
--- ex00/vendor/imgconv/convert.go
+++ ex00/vendor/imgconv/convert.go
@@ -8,8 +8,6 @@ import (
 	_ "image/jpeg"
 	"image/png"
 	"os"
-	// "strings"
-	// "path/filepath"
 )
 
 type filePath struct {
@@ -39,7 +37,6 @@ func Convert(path, iExt, oExt string) (err error) {
 	}
 
 	// 変換先のファイルパスを取得
-	// fp.png = convertExtension(fp.jpg, "jpg", "png")
 	fp.out = replaceExt(fp.in, iExt, oExt)
 
 	// 変換先のファイルを作成
@@ -62,57 +59,6 @@ func Convert(path, iExt, oExt string) (err error) {
 	return nil
 }
 
-// jpgファイルをpngファイルに変換する関数
-// func ConvertJpgToPng(jpg string) (err error) {
-// 	var fp filePath
-// 	fp.jpg = jpg
-
-// 	if err := CheckJpg(fp.jpg); err != nil {
-// 		return err
-// 	}
-
-// 	// 変換元ファイルを開く
-// 	f, err := os.Open(fp.jpg)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer f.Close()
-
-// 	// 変換元ファイルを画像オブジェクトに変換
-// 	img, _, err := image.Decode(f)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// 変換先のファイルパスを取得
-// 	// fp.png = convertExtension(fp.jpg, "jpg", "png")
-// 	fp.png = replaceExt(fp.jpg, ".jpg", ".png")
-
-// 	// 変換先のファイルを作成
-// 	f2, err := os.Create(fp.png)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer func () {
-// 		if e := f2.Close(); e != nil {
-// 			err = e
-// 		}
-// 	}()
-
-// 	// 画像オブジェクトをpng形式にエンコード
-// 	err = png.Encode(f2, img)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
 func replaceExt(path, iExt, oExt string) string {
 	return path[:len(path)-len(iExt)] + oExt
 }
-
-// func convertExtension(path, fromExt, toExt string) string {
-// 	r := strings.Replace(reverse(path), reverse(fromExt), reverse(toExt), 1)
-// 	return reverse(r)
-// }
